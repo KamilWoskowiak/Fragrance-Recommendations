3d9736985e9d250e23eab35df84d3a130c7fede6
KamilWoskowiak <kamilwoskowiak1@gmail.com>
2025-02-05 18:23:56 -0500

    script done

--- DataCollectionProccessing/dataGroups.py
+++ DataCollectionProccessing/dataGroups.py
@@ -14,8 +14,15 @@ csv_filename = "fragrance_list_brands.csv"
 def fetch(link, proxies):
     return requests.get(link, proxies=proxies, verify=False)
 
+def countRows(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        return sum(1 for _ in enumerate(f))
+
 
 def processCsv(inputFile, outputFile, proxies):
+
+    numRows = countRows(inputFile)-1
+
     with open(inputFile, mode='r', encoding='utf-8') as infile, open(outputFile, mode='w', newline='', encoding='utf-8') as outfile:
         reader = csv.reader(infile)
         writer = csv.writer(outfile)
@@ -24,15 +31,23 @@ def processCsv(inputFile, outputFile, proxies):
 
         outputHeaders = extractData(None).keys()
         writer.writerow(outputHeaders)
+        print(outputHeaders)
 
         for i, row in enumerate(reader):
             link = row[2]
+            try:
+                html = fetch(link, proxies)
+                extractedData = extractData(html)
+                writer.writerow([extractedData.get(key, "") for key in outputHeaders])
+            except:
+                print(f"ERROR AT INDEX {i} NAMED {str(row[0:2])}")
+                input("Continue?")
+                writer.writerow([row[0], row[1]])
 
-            html = fetch(link, proxies)
-            extractedData = extractData(html)
-            writer.writerow([extractedData.get(key, "") for key in outputHeaders])
-            print(f"Completed row {i}")
+            print(f"Completed row {i} : {i/numRows*100}%")
+            print("Sleeping...")
             time.sleep(2)
+            print("Awake...")
 
 proxies = {
     "https": os.getenv("PROXY_URL")
